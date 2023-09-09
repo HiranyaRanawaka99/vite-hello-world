9526aca285b72e824306f338d369b1d882783f22
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-09-09 15:10:48 +0300

    add: h1 and jumbotron

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+  import AppHero from './components/AppHero.vue';
+  export default {
+    data() {
+      return {
+        title: 'La mia prima App con vite',
+      }
+    },
+
+    components : { AppHero }
+  }
+</script>
+
+
+<template>
+  <div class="container">
+    <h1> {{ title }}</h1>
+
+    <AppHero></AppHero>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  h1 {
+    text-align: center;
+    font-size: 3rem;
+  }
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,33 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+    }
+</script>
+
+<template>
+    
+    <div class="jumbotron"> 
+        <p>JUMBOTRON</p>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+.jumbotron {
+    width: 100vw;
+    height: 400px;
+    background-color: burlywood;
+    
+    p {
+        color: red;
+        font-size: 1.5rem;
+        text-align: center;
+        padding-top: 200px;
+    }
+}
+</style>
